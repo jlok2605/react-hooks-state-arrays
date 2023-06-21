8da654e6eba82bca6171b3b4c29a789243fa0d1f
Jeremy Lok <lambourghinis_pone@hotmail.com>
2023-06-21 11:24:50 +1000

    assignment 2

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { spicyFoods, getNewRandomSpicyFood } from "../data";
-
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
 
@@ -28,6 +27,30 @@ function SpicyFoodList() {
     </li>
   ));
 
+const [filterBy, setFilterBy] = useState ("All");
+function handleFilterChange(event) {{
+  setFilterBy(event.target.value);
+}
+const [foods, setFoods] = useState(spicyFoods);
+const [filterBy, setFilterBy] = useState("All");
+
+const foodsToDisplay = foods.filter((food) => {
+  if (filterBy === "All") {
+    return true;
+  } else {
+    return food.cuisine === filterBy;
+  }
+});
+return (
+  <select name="filter" onChange={handleFilterChange}>
+    <option value="All">All</option>
+    <option value="American">American</option>
+    <option value="Sichuan">Sichuan</option>
+    <option value="Thai">Thai</option>
+    <option value="Mexican">Mexican</option>
+  </select>
+);
+}
 
   return (
     <div>
